f828cd1d8d13332e0498311d492198e028ca0699
Richard J. Sove <rjsove@gmail.com>
2020-02-17 21:58:05 -0500

    added sparse.cpp

---
+++ INF/src/sparse.cpp
@@ -0,0 +1,14 @@
+#include "sparse.h"
+
+// Contructor
+pentadiagonal::pentadiagonal()
+{
+}
+
+pentadiagonal::pentadiagonal(int n)
+{
+  
+}
+
+
+// Destructor
---
+++ INF/src/sparse.h
@@ -0,0 +1,22 @@
+#ndef SPARSE_H
+#define SPARSE_H
+
+#include <iostream>
+using namespace std;
+
+class pentadiagonal 
+{
+public:
+  pentadiagonal();
+  ~pentadiagonal();
+  void setP(int l);
+  void setN(int l);
+  void setE(int l);
+  void setS(int l);
+  void setW(int l);
+private:
+  int N;
+  double* P,N,E,S,W;
+};
+
+#endif
